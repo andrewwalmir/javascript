58c7f931676baa12a5e94c59469c9c685531de03
andrewwalmir <andrewwalmir@gmail.com>
2020-10-17 00:22:51 -0300

    Auula 02 - Arrow Function

--- 01-tudo-e-objeto/scripts.js
+++ 01-tudo-e-objeto/scripts.js
@@ -1,19 +1,31 @@
-// Objeto Literal
-const menu = {
-  selector: ".principal",
-  active() {
-    const menuElement = document.querySelector(this.selector);
-    menuElement.classList.add("active");
+const upperName = function(name) {
+    return name.toUpperCase();
+  };
+  upperName("André"); // ANDRÉ
+  
+  const lowerName = name => {
+    return name.toLowerCase();
+  };
+  lowerName("Rafael"); // rafael
+  
+  const countLetters = word => word.length;
+  countLetters("Rafael"); // 6
+
+  
+
+  class Menu {
+    constructor(menu) {
+      this.menuElement = document.querySelector(menu);
+      this.activeClass = "active";
+    }
+    addActiveEvent() {
+      // sem o arrow function ele não iria funcionar
+      this.menuElement.addEventListener("click", event =>
+        event.target.classList.add(this.activeClass)
+      );
+    }
   }
-};
-
-menu.selector; // ".principal";
-menu.active(); // adicionar active ao menu
-menu.hasOwnProperty("class"); // método herdado
-
-
-["10", "20", "30"].map(Number); // [10, 20, 30];
-"JavaScript".toUpperCase(); // JAVASCRIPT
-
-const body = document.querySelector("body");
-body.classList.add("js"); // adiciona JS ao Body
+  
+  const menu = new Menu(".principal");
+  menu.addActiveEvent();
+  
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # javascript
+
+
+JAVASCRIPT ANTES DO FRAMEWORK
+<br>
+O que aprender antes de iniciar com Vue.js ou React
+<br>
+[Origamid](https://www.youtube.com/watch?v=j6iSONAO6sQ&list=PL9rc_FjKlX39T78CUANwmdta_d1CgUtMt)
